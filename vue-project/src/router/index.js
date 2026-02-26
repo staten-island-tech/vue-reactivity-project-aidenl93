1721d52eccbd4c27969cb9a0181f4eb0e10d7210
aidenl93 <aidenl93@nycstudents.net>
2026-02-26 13:52:43 -0500

    mr whalen do you want a plate

--- vue-project/src/router/index.js
+++ vue-project/src/router/index.js
@@ -1,8 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import shoes from '@/components/shoes.vue'
+import app from '../views/App.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [{ path: '/', name: 'shoes', component: shoes }],
+  routes: [{ path: '/', name: 'app', component: app }],
 })
 
 export default router
